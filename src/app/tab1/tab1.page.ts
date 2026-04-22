79127ede4129e10b5645933bf27481df5d9d7d10
darliordonez9-hash <darliordonez9@gmail.com>
2026-04-22 14:26:40 -0600

    Add files via upload

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/angular/standalone';
+
+import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss'],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle],
+})
+export class Tab1Page {
+  constructor() {}
+}
